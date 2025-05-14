62d74c940723eb7286baf0b80dc989138b0423d9
uncletientrung <thihachcf@gmail.com>
2025-05-14 23:42:46 +0700

    update lai code cho output ra phan classification

---
+++ src/demo_release_eccv16.py
@@ -0,0 +1,41 @@
+import argparse
+import matplotlib.pyplot as plt
+
+from colorizers import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i','--img_path', type=str, default='src/imgs/scale.jpg')
+parser.add_argument('--use_gpu', action='store_true', help='whether to use GPU')
+parser.add_argument('-o','--save_prefix', type=str, default='saved', help='will save into this file with eccv16.png suffix')
+opt = parser.parse_args()
+
+# load colorizer
+colorizer_eccv16 = eccv16(pretrained=True).eval()
+if(opt.use_gpu):
+    colorizer_eccv16.cuda()
+
+# default size to process images is 256x256
+# grab L channel in both original ("orig") and resized ("rs") resolutions
+img = load_img(opt.img_path)
+(tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256,256))
+if(opt.use_gpu):
+    tens_l_rs = tens_l_rs.cuda()
+
+# colorizer outputs 256x256 ab map
+# resize and concatenate to original L channel
+img_bw = postprocess_tens(tens_l_orig, torch.cat((0*tens_l_orig,0*tens_l_orig),dim=1))
+out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
+
+plt.imsave('%s_eccv16.png'%opt.save_prefix, out_img_eccv16)
+
+plt.figure(figsize=(8,4))
+plt.subplot(1,2,1)
+plt.imshow(img_bw)
+plt.title('Input')
+plt.axis('off')
+
+plt.subplot(1,2,2)
+plt.imshow(out_img_eccv16)
+plt.title('Output')
+plt.axis('off')
+plt.show()
